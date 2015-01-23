7e30f88b52218e2b291d5c894f2c710d7ffe9d95
david <ddambreville@aldebaran-robotics.com>
2015-01-23 14:26:57 +0100

    [PaNiProblem] modification of the syntax of the script

--- PaNiProblem.py
+++ PaNiProblem.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 #==============================================================================
-#               ALDEBARAN-ROBOTICS - SOFTWARE QA
+#               ALDEBARAN-ROBOTICS - SOFTWARE QH
 #==============================================================================
 # FILE : PaNiProblem.py
 # DESCRIPTION
@@ -25,25 +24,10 @@ __email__ = "ddambreville@aldebaran.com"
 import ConfigParser
 import argparse
 import time
-import sys
-import os
-import stat
 from threading import Thread
 from naoqi import ALProxy
 
 DEFAULT_IP = "127.0.0.1"
-try:
-    path = os.getcwd()
-    os.chmod(path, stat.S_IXGRP)
-except:
-    pass
-'''
-path = os.getcwd()
-print "path : " + path
-os.path.basename(path)
-# sys.path.append(path)
-#files = os.listdir(path)
-'''
 
 
 def read_section(my_file, section):
@@ -65,16 +49,16 @@ def read_section(my_file, section):
         return {}
 
 
-def serial_ID(mem):
+def serial_id(mem):
     """Head and Body ID"""
-    head_ID = mem.getData('RobotConfig/Head/FullHeadId')
-    body_ID = mem.getData('Device/DeviceList/ChestBoard/BodyId')
-    print "Body ID: " + body_ID
-    print "Head ID: " + head_ID
+    head_id = mem.getData('RobotConfig/Head/FullHeadId')
+    body_id = mem.getData('Device/DeviceList/ChestBoard/BodyId')
+    print "Body ID: " + body_id
+    print "Head ID: " + head_id
     return
 
 
-def Head_Config(mem):
+def head_config(mem):
     """Read Head's configuration key from DeviceHead"""
     # Variable and List
     prefix = 'RobotConfig/Head/Device/'
@@ -99,15 +83,15 @@ def Head_Config(mem):
                 key_list = (key, key_description.replace('_', ' '))
                 final_key_list.append(key_list)
                 longg_head = dict(
-                    zip((0, 1), (len(str(x)) for x in header_head)))
+                    zip((0, 1), (len(str(lenght)) for lenght in header_head)))
 
     # Distribute the data in a table
-    for tu in final_key_list:
-        for x in final_key_list:
+    for key in final_key_list:
+        for lenght in final_key_list:
             longg_head.update(
                 (i, max(longg_head[i], len(str(el))))
-                for i, el in enumerate(tu))
-            longg_head[1] = max(longg_head[1], len(str(x)))
+                for i, el in enumerate(key))
+            longg_head[1] = max(longg_head[1], len(str(lenght)))
     fofo_error = ' | '.join('%%-%ss' % longg_head[i] for i in xrange(0, 2))
     print '\n'.join((fofo_error % header_head,
                      '-|-'.join(longg_head[i] * '-' for i in xrange(2)),
@@ -116,15 +100,14 @@ def Head_Config(mem):
     return
 
 
-def Body_Config(mem):
+def body_config(mem):
     """Read Body's configuration key from DeviceBody"""
     # Variable and List
     prefix = 'RobotConfig/Body/Device/'
     header_body = ('Key', 'Description')
     final_key_body = []
     key_list = ['LeftHand', 'RightHand', 'LeftArm', 'RightArm',
-                'Legs', 'Brakes', 'Wheels', 'Platform', 'Version',
-                'Calibration']
+                'Legs', 'Brakes', 'Wheel', 'Platform', 'Version']
 
     # Exception
     for key in key_list:
@@ -135,30 +118,37 @@ def Body_Config(mem):
             key = 'Hand/Right'
         elif key == 'Version':
             key = 'BaseVersion'
-        elif key == 'Calibration':
-            key = 'Joints/Calibration'
-        try:
-            if key == 'BaseVersion':
-                key_value = mem.getData(prefix.replace('Device/', '') + key)
-            else:
-                key_value = mem.getData(prefix + key + '/Version')
-        except:
-            pass
+
+        if key == 'BaseVersion':
+            key_value = mem.getData(prefix.replace('Device/', '') + key)
+            if not key_value:
+                key_value = 0
+        else:
+            key_value = mem.getData(prefix + key + '/Version')
+            if not key_value:
+                key_value = 0
+
         for check in key_check:
             if check == key_value:
                 key_description = key_check[check][0]
                 key_list = (key, key_description.replace('_', ' '))
                 final_key_body.append(key_list)
                 longg_body = dict(
-                    zip((0, 1), (len(str(x)) for x in header_body)))
+                    zip((0, 1), (len(str(lenght)) for lenght in header_body)))
+            elif key_value == 0:
+                key_description = 'Unknown key'
+                key_list = (key, key_description)
+                final_key_body.append(key_list)
+                longg_body = dict(
+                    zip((0, 1), (len(str(lenght)) for lenght in header_body)))
 
     # Distribute the data in a table
-    for tu in final_key_body:
-        for x in final_key_body:
+    for key in final_key_body:
+        for lenght in final_key_body:
             longg_body.update(
                 (i, max(longg_body[i], len(str(el))))
-                for i, el in enumerate(tu))
-            longg_body[1] = max(longg_body[1], len(str(x)))
+                for i, el in enumerate(key))
+            longg_body[1] = max(longg_body[1], len(str(lenght)))
     fofo_error = ' | '.join('%%-%ss' % longg_body[i] for i in xrange(0, 2))
     print '\n'.join((fofo_error % header_body,
                      '-|-'.join(longg_body[i] * '-' for i in xrange(2)),
@@ -203,6 +193,8 @@ def trad_serial(mem):
 
 class Nack(Thread):
 
+    """Check Nack on robot (sample of 5s)"""
+
     def __init__(self, board_list, mem):
         Thread.__init__(self)
         self.board_list = board_list
@@ -218,17 +210,20 @@ class Nack(Thread):
         second_nack = self.mem.getListData(
             ["/".join([prefix, board, suffix]) for board in self.board_list])
 
-        for index, (board, NackMax, NackMin) in \
+        for index, (board, nackmax, nackmin) in \
                 enumerate(zip(self.board_list, second_nack, first_nack)):
-            value = (NackMax - NackMin)
+            value = (nackmax - nackmin)
             self.nack_status.append(value)
 
         # print self.nack_status
     def result(self):
+        """return Nack value"""
         return self.nack_status
 
 
 class Ack(Thread):
+
+    """Check Ack on robot (sample of 5s)"""
 
     def __init__(self, board_list, mem):
         Thread.__init__(self)
@@ -246,22 +241,27 @@ class Ack(Thread):
         last_ack = self.mem.getListData(
             ["/".join([prefix, board, suffix]) for board in self.board_list])
 
-        for index, (board, AckMax, AckMin) \
+        for index, (board, ackmax, ackmin) \
                 in enumerate(zip(self.board_list, last_ack, first_ack)):
-            value = (AckMax - AckMin)  # /30
+            value = (ackmax - ackmin)  # /30
             self.ack_value.append(value)
 
     def result(self):
+        """return Ack value"""
         return self.ack_value
 
 
 class Error(Thread):
+
+    """Check Error on robot (sample of 5s)"""
 
     def __init__(self, board, mem, prefix):
         Thread.__init__(self)
         self.board = board
         self.mem = mem
         self.prefix = prefix
+        self.unknown_error = "UNKNOWN ERROR"
+        self.error_description = []
 
     def run(self):
         upper_list = ['HeadBoard', 'LeftArmBoard', 'LeftHandBoard',
@@ -271,9 +271,6 @@ class Error(Thread):
                      'HipBoard', 'HubBoardPlatform', 'ThighBoard']
         laser_list = ['LaserSensorFrontPlatform',
                       'LaserSensorLeftPlatform', 'LaserSensorRightPlatform']
-        board_error = read_section("Config_joint_board_pepper.cfg", "Board")
-        self.error_description = []
-        error_description = "UNKNOWN ERROR"
         i = 0
         for i in range(0, 5):
             if self.board in upper_list:
@@ -284,7 +281,7 @@ class Error(Thread):
                     if int(code_error) != 0:
                         if int(code_error) != int(error):
                             error_description = str(
-                                code_error) + " " + error_description
+                                code_error) + " " + self.unknown_error
                             self.error_description.append(
                                 error_description)
                         else:
@@ -302,7 +299,7 @@ class Error(Thread):
                     if int(code_error) != 0:
                         if int(code_error) != int(error):
                             error_description = str(
-                                code_error) + " " + error_description
+                                code_error) + " " + self.unknown_error
                             self.error_description.append(
                                 error_description)
                         else:
@@ -311,7 +308,7 @@ class Error(Thread):
                             self.error_description.append(error_description)
                         i = i + 1
 
-            else:
+            elif self.board in laser_list:
                 error_list = read_section("Error_list.cfg", "Laser")
                 code_error = self.mem.getData(
                     self.prefix + str(self.board) + "/Error")
@@ -320,7 +317,7 @@ class Error(Thread):
                     if int(code_error) != 0:
                         if int(code_error) != int(error):
                             error_description = str(
-                                code_error) + " " + error_description
+                                code_error) + " " + self.unknown_error
                             self.error_description.append(
                                 error_description)
                         else:
@@ -330,33 +327,39 @@ class Error(Thread):
                         i = i + 1
 
     def result(self):
+        """return error description"""
         return self.error_description
 
 
-class Firmware_version(Thread):
+class FirmwareVersion(Thread):
+
+    """Check firmware version for all board"""
 
     def __init__(self, board, mem, prefix):
         Thread.__init__(self)
         self.board = board
         self.mem = mem
         self.prefix = prefix
+        self.firmware_version = ''
 
     def run(self):
         self.firmware_version = self.mem.getData(
             self.prefix + str(self.board) + '/ProgVersion')
 
     def result(self):
+        """return version of firmware"""
         return self.firmware_version
 
 
 def info_battery(mem):
+    """Check Battry information (charge)"""
     info = mem.getData('BatteryChargeChanged')
     print 'Battery percent level : ' + str(info) + ' %'
     return
 
 
 def main():
-
+    """Main program"""
     #[Argument Parser]---------------------------------------------------------
     parser = argparse.ArgumentParser(description='Information from Robot')
 
@@ -372,9 +375,6 @@ def main():
     args = parser.parse_args()
 
     #[VARIABLE]----------------------------------------------------------------
-    firmware_version = 0
-    nack_status = "ok"
-    ack_value = 0
     final_list = []
     final_error = []
     final_ack_list = []
@@ -418,7 +418,7 @@ def main():
     print "Naoqi Version : " + naoqi_version
 
     # Head and Body ID
-    serial_ID(mem)
+    serial_id(mem)
 
     # Serial
     trad_serial(mem)
@@ -431,11 +431,11 @@ def main():
     print "#############################################################"
 
     # Body Config
-    Body_Config(mem)
+    body_config(mem)
 
     # Head Config
     print'\n'
-    Head_Config(mem)
+    head_config(mem)
 
     print "\n#############################################################"
     print "#                       Robot Analysis                      #"
@@ -447,7 +447,7 @@ def main():
     thread_nack.start()
     for board in enumerate(board_list):
         thread_error = Error(board[1], mem, prefix)
-        thread_firmware = Firmware_version(board[1], mem, prefix)
+        thread_firmware = FirmwareVersion(board[1], mem, prefix)
 
         # Start thread
         thread_error.start()
@@ -481,12 +481,14 @@ def main():
                 pass
             error_header = ('Board', 'Error description')
             longg_error = dict(
-                zip((0, 1), (len(str(x)) for x in error_header)))
-        except:
+                zip((0, 1), (len(str(lenght_error)) for lenght_error
+                             in error_header)))
+        except Exception:
             pass
         header = ('Board', 'Firmware Version')
         final_list.append(value_list)
-        longg = dict(zip((0, 1), (len(str(x)) for x in header)))
+        longg = dict(zip((0, 1), (len(str(lenght_gen))
+                                  for lenght_gen in header)))
 
     for index, (board, ack, nack) in enumerate(zip(board_list, ack_result,
                                                    nack_result)):
@@ -498,15 +500,16 @@ def main():
         final_ack_list.append(ack_nack_list)
 
     header_ack_nack = ('Board', 'Ack (5s)', 'Nack Status', 'Ack/Nack (%)')
-    longg_ack = dict(zip((0, 1, 2, 3), (len(str(x)) for x in header_ack_nack)))
+    longg_ack = dict(zip((0, 1, 2, 3), (len(str(lenght_ack))
+                                        for lenght_ack in header_ack_nack)))
 
     # table ack
     for val in final_ack_list:
-        for x in final_ack_list:
+        for lenght_ack in final_ack_list:
             longg_ack.update(
                 (i, max(longg_ack[i], len(str(el)))) for i, el
                 in enumerate(val))
-            longg_ack[3] = max(longg_ack[3], len(str(x)))
+            longg_ack[3] = max(longg_ack[3], len(str(lenght_ack)))
     fofo = ' | '.join('%%-%ss' % longg_ack[i] for i in xrange(0, 4))
     print '\n'.join((fofo % header_ack_nack,
                      '-|-'.join(longg_ack[i] * '-' for i in xrange(4)),
@@ -515,11 +518,11 @@ def main():
     print '\n'
 
     # table
-    for tu in final_list:
-        for x in final_list:
+    for key in final_list:
+        for lenght_gen in final_list:
             longg.update((i, max(longg[i], len(str(el))))
-                         for i, el in enumerate(tu))
-            longg[1] = max(longg[1], len(str(x)))
+                         for i, el in enumerate(key))
+            longg[1] = max(longg[1], len(str(lenght_gen)))
     fofo = ' | '.join('%%-%ss' % longg[i] for i in xrange(0, 2))
     print '\n'.join((fofo % header,
                      '-|-'.join(longg[i] * '-' for i in xrange(2)),
@@ -530,12 +533,12 @@ def main():
     print "#############################################################"
     try:
         # table error
-        for tu in final_error:
-            for x in final_error:
+        for error_key in final_error:
+            for lenght_error in final_error:
                 longg_error.update(
                     (i, max(longg_error[i], len(str(el)))) for i, el
-                    in enumerate(tu))
-                longg_error[1] = max(longg_error[1], len(str(x)))
+                    in enumerate(error_key))
+                longg_error[1] = max(longg_error[1], len(str(lenght_error)))
         fofo_error = ' | '.join(
             '%%-%ss' % longg_error[i] for i in xrange(0, 2))
         print '\n' + '\n'.join((fofo_error % error_header,
@@ -543,9 +546,8 @@ def main():
                                            * '-' for i in xrange(2)),
                                 '\n'.join(fofo_error % (a, b) for (a, b)
                                           in final_error)))
-    except:
+    except Exception:
         print '\nNo error on your robot'
-        pass
 
 if __name__ == '__main__':
     main()
